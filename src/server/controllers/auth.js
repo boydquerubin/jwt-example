56d3b33b0f0c00624bea546ffeed635338582288
Boyd Querubin <boydisaacq@gmail.com>
2024-06-12 06:51:39 -0600

    auth and env

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -1,15 +1,38 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const SECRET = process.env.SECRET;
 
-const generateToken = () => {
-
-}
+const generateToken = (info) => {
+  return jwt.sign(
+    {
+      //information for encoding
+      username: info.username,
+      email: info.email,
+    },
+    SECRET,
+    {
+      //options
+      expiresIn: "5 minutes",
+    }
+  );
+};
 
 module.exports = {
   createToken: async (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
+    let token = generateToken(req.body);
+    res.status(200).send(token);
   },
 
   validateToken: async (req, res) => {
-    console.log(req.body)
+    let token = req.get("Authorization");
+    let valid = jw.verify(token, SECRET);
+
+    if (valid) {
+      res.status(200).send("Success");
+    } else {
+      res.status(400).send("Invalid...");
+    }
+    console.log(req.body);
   },
-}
+};
